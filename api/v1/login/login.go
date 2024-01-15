de2aedd0493d987e173b32bbda9e8a69dc41518e
van_liu <826199528@qq.com>
2024-01-15 17:43:40 +0800

    对登录接口进行简单封装

--- api/v1/login/login.go
+++ api/v1/login/login.go
@@ -8,7 +8,9 @@ import (
 	"time"
 	"xz-go-frame/commons/jwtgo"
 	"xz-go-frame/commons/response"
+	"xz-go-frame/model/user"
 	service "xz-go-frame/service/user"
+	"xz-go-frame/utils"
 )
 
 // 登录业务
@@ -72,34 +74,59 @@ func (api *LoginApi) ToLogined(c *gin.Context) {
 
 	// 校验用户的账号密码输入是否和数据库一致
 	if dbUser != nil && dbUser.Password == inputPassword {
-		// 1、jwt 生成token
-		myJwt := jwtgo.NewJWT()
-		// 2、生成token
-		token, err2 := myJwt.CreateToken(jwtgo.CustomClaims{
-			dbUser.ID,
-			dbUser.Name,
-			int64(1545),
-			jwt.RegisteredClaims{
-				Audience:  jwt.ClaimStrings{"XZ-USER"},                                            // 受众
-				Issuer:    "MWJ-ADMIN",                                                            // 签发者
-				IssuedAt:  jwt.NewNumericDate(time.Now()),                                         // 签发时间
-				NotBefore: jwt.NewNumericDate(time.Now().Add(-1000)),                              // 生效时间
-				ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Second * 60 * 60 * 24 * 7)), // 过期时间 7天
-			},
-		})
-		fmt.Println("当前时间是：", time.Now().Unix())
-		fmt.Println("签发时间：" + time.Now().Format("2006-01-02 15:04:05"))
-		fmt.Println("生效时间：" + time.Now().Add(-1000*time.Second).Format("2006-01-02 15:04:05"))
-		fmt.Println("过期时间：" + time.Now().Add(1*time.Second*60*60*24*7).Format("2006-01-02 15:04:05"))
-		if err2 != nil {
-			response.Fail(60002, "登录失败,token颁发不成功！", c)
-			return
-		}
-		response.Ok(map[string]any{"user": dbUser, "token": token}, c)
+		token := api.generaterToken(c, dbUser)
+		// 根据用户id查询用户的角色
+		roles := [2]map[string]any{}
+		m1 := map[string]any{"id": 1, "name": "超级管理员"}
+		m2 := map[string]any{"id": 2, "name": "财务"}
+		roles[0] = m1
+		roles[1] = m2
+		// 根据用户id查询用户的角色的权限
+		permissions := [2]map[string]any{}
+		pm1 := map[string]any{"code": 10001, "name": "保存用户"}
+		pm2 := map[string]any{"code": 20001, "name": "删除用户"}
+		permissions[0] = pm1
+		permissions[1] = pm2
+
+		response.Ok(map[string]any{"user": dbUser, "token": token, "roles": roles, "permissions": permissions}, c)
 
 	} else {
 		response.Fail(60002, "你输入的账号和密码有误", c)
 	}
+}
+
+/*
+根据用户信息创建一个token
+*/
+func (api *LoginApi) generaterToken(c *gin.Context, dbUser *user.User) string {
+	// 设置token续期的缓冲时间
+	bf, _ := utils.ParseDuration("1d")
+	ep, _ := utils.ParseDuration("7d")
+
+	// 1、jwt 生成token
+	myJwt := jwtgo.NewJWT()
+	// 2、生成token
+	token, err2 := myJwt.CreateToken(jwtgo.CustomClaims{
+		dbUser.ID,
+		dbUser.Name,
+		int64(bf / time.Second),
+		jwt.RegisteredClaims{
+			Audience:  jwt.ClaimStrings{"XZ-USER"},               // 受众
+			Issuer:    "MWJ-ADMIN",                               // 签发者
+			IssuedAt:  jwt.NewNumericDate(time.Now()),            // 签发时间
+			NotBefore: jwt.NewNumericDate(time.Now().Add(-1000)), // 生效时间
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ep)),    // 过期时间 7天
+		},
+	})
+	fmt.Println("当前时间是：", time.Now().Unix())
+	fmt.Println("缓冲时间是：", int64(bf/time.Second))
+	fmt.Println("签发时间：" + time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("生效时间：" + time.Now().Add(-1000).Format("2006-01-02 15:04:05"))
+	fmt.Println("过期时间：" + time.Now().Add(ep).Format("2006-01-02 15:04:05"))
+	if err2 != nil {
+		response.Fail(60002, "登录失败,token颁发不成功！", c)
+	}
+	return token
 }
 
 // Login 用session时候的代码
