6b0cea72473573091f528499a4b91ae484c7c828
van_liu <826199528@qq.com>
2024-01-30 17:00:29 +0800

    更新jwt模型数据表

--- model/entity/jwt/jwt.go
+++ model/entity/jwt/jwt.go
@@ -4,15 +4,9 @@
 */
 package jwt
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
+import "xz-go-frame/global"
 
 type JwtBlacklist struct {
-	ID        uint           `gorm:"primarykey;comment:主键ID"` // 主键ID
-	CreatedAt time.Time      `gorm:"type:datetime(0);comment:创建时间" json:"createdAt"`
-	UpdatedAt time.Time      `gorm:"type:datetime(0);comment:更新时间" json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"index;comment:删除时间" json:"-"` // 删除时间
-	Jwt       string         `gorm:"type:text;comment:jwt"`
+	global.GVA_MODEL
+	Jwt string `gorm:"type:text;comment:jwt"`
 }
