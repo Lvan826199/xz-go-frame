a57848505faad8247bdfffc83fee0a02fcd88daf
van_liu <826199528@qq.com>
2024-03-07 17:10:11 +0800

    api层video代码提交

--- api/v1/video/enter.go
+++ api/v1/video/enter.go
@@ -4,6 +4,20 @@
 */
 package video
 
+import "xz-go-frame/service"
+
 type WebApiGroup struct {
-	Video
+	XkVideoCategoryApi
+	XkVideoApi
 }
+
+var (
+	VALIDATOR_MAP        = map[string]string{"code": "701", "msg": "验证属性有误"}
+	BINDING_PAMATERS_MAP = map[string]string{"code": "702", "msg": "参数有误"}
+)
+var (
+	// 课程分类
+	xkcategoryService = service.ServiceGroupApp.XkVideoServiceGroup.VideoCategoryService
+	// 课程
+	xkVideoService = service.ServiceGroupApp.XkVideoServiceGroup.VideoService
+)
