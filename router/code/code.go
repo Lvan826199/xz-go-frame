8cce96f6a7c366e77bb1eeed9bb1c7c74e0adf45
van_liu <826199528@qq.com>
2024-03-08 18:11:36 +0800

    router层code代码

--- router/code/code.go
+++ router/code/code.go
@@ -6,18 +6,18 @@ package code
 
 import (
 	"github.com/gin-gonic/gin"
-	"xz-go-frame/api/v1/code"
+	"xz-go-frame/api/v1"
 )
 
 type CodeRouter struct{}
 
-func (e *CodeRouter) InitCodeRouter(Router *gin.Engine) {
-	codeApi := code.CodeApi{}
-	// 这个路由多了一个对post\put请求的中间件处理，而这个中间件做了一些对post和put参数的处理和一些公共信息的处理
-	coureseRouter := Router.Group("code")
-	{
-		coureseRouter.GET("get", codeApi.CreateCaptcha)
-		coureseRouter.GET("verify", codeApi.VerifyCaptcha)
-	}
+func (r *CodeRouter) InitCodeRouter(Router *gin.RouterGroup) {
 
+	codeApi := v1.WebApiGroupApp.Code.CodeApi
+	// 这个路由多了一个对对post，put请求的中间件处理，而这个中间件做了一些对post和put的参数的处理和一些公共信息的处理
+	router := Router.Group("code")
+	{
+		router.GET("get", codeApi.CreateCaptcha)
+		router.GET("verify", codeApi.VerifyCaptcha)
+	}
 }
---
+++ router/code/enter.go
@@ -0,0 +1,9 @@
+/*
+* @Author: 梦无矶小仔
+* @Date: 2024/3/8 17:39
+ */
+package code
+
+type WebRouterGroup struct {
+	CodeRouter
+}
